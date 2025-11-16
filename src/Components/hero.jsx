241439e4ea07855df9accf411bd52e137d8c7c30
NourSaneh <noursaneh34@gmail.com>
2025-11-17 03:19:16 +0400

    vercel-test

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -1,9 +1,9 @@
-import mainBG from "../assets/main.png";
+import herobg from "../assets/herobg.png";
 export default function Hero() {
     return (
 <section
   className="w-full h-[90vh] bg-center bg-cover bg-no-repeat"
-  style={{ backgroundImage: `url(${mainBG})` }}
+  style={{ backgroundImage: `url(${herobg})` }}
 >
   <div className="flex items-center justify-center h-full">
     <h1 className="text-white text-4xl font-bold">Adventure awaits at every<br></br> turn of the Trail</h1>
